7e532b014c3c39cc7d530d6cef2b51232bad2606
Scripting With Saad <muhammadsaad965000@gmail.com>
2024-07-08 10:05:18 +0500

    adding javascript folder

---
+++ javascript/script.js
@@ -0,0 +1,13 @@
+
+
+const clock = document.querySelector("#clock")
+
+// let date = new Date();
+// console.log(date.toLocaleTimeString())
+
+setInterval(function () {
+
+    let date = new Date();
+    clock.innerHTML = date.toLocaleTimeString();
+
+}, 1000)
